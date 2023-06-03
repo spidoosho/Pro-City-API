3fcc605342746b833cd8cc5600d16c5cfaf05e78
Dúšis <hoangant@lab.ms.mff.cuni.cz>
2023-06-03 15:41:41 +0200

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,44 @@
-const express = require("express");
-const router = express.Router();
+var express = require('express');
+/*
+import { NextResponse } from 'next/server';
+import { get } from '@vercel/edge-config';
+*/
 
-router.get("/", async (req, res, next) => {
-  return res.status(200).json({
-    title: "Express Testing",
-    message: "The app is working properly!",
-  });
+var router = express.Router();
+
+var leaderboard = null;
+
+async function getLeaderboardFromDB(){
+    //const leaderboard = await get('leaderboard');
+    return null;
+    //return NextResponse.json(leaderboard);
+}
+
+router.get('/leaderboard', async function (req, res) {
+    if (leaderboard == null) {
+        leaderboard = await getLeaderboardFromDB();
+    } 
+
+    res.json(leaderboard)
+});
+
+
+// get player
+router.get('/player/:username(.+)', function (req, res) {
+    var currPlayer = leaderboard.filter(player => player.username == req.params.username);
+
+    if (currPlayer.length == 1) {
+        res.json(currPlayer[0])
+    } else {
+        res.status(404);
+        res.json({ message: "Not Found" });
+    }
+});
+
+router.put('/leaderboard', async function (req, res) {
+    leaderboard = await getLeaderboardFromDB();
+
+    res.json({message: "Leaderboard updated.", location: "/leaderboard"});
 });
 
 module.exports = router;
