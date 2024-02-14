485494583f1c77f77e2d040371bf22fc2e0ec8df
Dúšis <hoangant@lab.ms.mff.cuni.cz>
2024-02-14 22:16:52 +0100

    added player

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -1,10 +1,9 @@
 var express = require('express');
 const { DynamoDBClient } = require('@aws-sdk/client-dynamodb')
-const { ListTablesCommand, ScanCommand, UpdateItemCommand, GetItemCommand, TransactWriteItemsCommand, PutItemCommand } = require('@aws-sdk/client-dynamodb')
+const { ScanCommand } = require('@aws-sdk/client-dynamodb')
 require('dotenv').config();
 
 var router = express.Router();
-
 const dbclient = new DynamoDBClient({
     region: "eu-north-1", 
     credentials: {
@@ -12,7 +11,35 @@ const dbclient = new DynamoDBClient({
       secretAccessKey: process.env.SECRET_ACCESS_KEY,
     },
   });
-  
+
+router.get('/leaderboard', async function (req, res) {
+    leaderboard = await getLeaderboardFromDB();
+    res.json(leaderboard)
+});
+
+router.get('/leaderboard/text', async function (req, res) {
+    leaderboard = await getLeaderboardFromDB();
+
+    if(leaderboard.length < 1) {
+        res.json("Leaderboard is empty");
+        return;
+    }
+
+    let result = `1. ${leaderboard[0].displayName.S} - ${leaderboard[0].elo.N} (${leaderboard[0].gamesWon.N}:${leaderboard[0].gamesLost.N})`
+
+    for(let i = 1; i < Math.min(leaderboard.length, 6); i++) {
+        result += `, ${i + 1}. ${leaderboard[i].displayName.S} - ${leaderboard[i].elo.N} (${leaderboard[i].gamesWon.N}:${leaderboard[i].gamesLost.N})`;
+    }
+
+    res.json(result)
+});
+
+router.get('/player/:username', async function (req, res) {
+    const player = await getPlayerDataFromDb(req.params.username)
+    const playerStr = `${player.displayName.S} - ${player.elo.N} (${player.gamesWon.N}: ${player.gamesLost.N})`
+    res.json(playerStr)
+});
+
 async function getLeaderboardFromDB() {
     function comparePlayers (a, b) {
         return parseInt(a.elo.N) - parseInt(b.elo.N)
@@ -43,46 +70,9 @@ async function getLeaderboardFromDB() {
     return leaderboard.sort(comparePlayers).reverse()
 }
 
-
-router.get('/leaderboard', async function (req, res) {
-    leaderboard = await getLeaderboardFromDB();
-    res.json(leaderboard)
-});
-
-router.get('/leaderboard/text', async function (req, res) {
-    leaderboard = await getLeaderboardFromDB();
-
-    if(leaderboard.length < 1) {
-        res.json("Leaderboard is empty");
-        return;
-    }
-
-    let result = `1. ${leaderboard[0].displayName.S} - ${leaderboard[0].elo.N} (${leaderboard[0].gamesWon.N}:${leaderboard[0].gamesLost.N})`
-
-    for(let i = 1; i < Math.min(leaderboard.length, 6); i++) {
-        result += `, ${i + 1}. ${leaderboard[i].displayName.S} - ${leaderboard[i].elo.N} (${leaderboard[i].gamesWon.N}:${leaderboard[i].gamesLost.N})`;
-    }
-
-    res.json(result)
-});
-
-
-// TODO
-router.get('/player/:username(.+)', function (req, res) {
-    var currPlayer = leaderboard.filter(player => player.username == req.params.username);
-
-    if (currPlayer.length == 1) {
-        res.json(currPlayer[0])
-    } else {
-        res.status(404);
-        res.json({ message: "Not Found" });
-    }
-});
-
-router.put('/leaderboard', async function (req, res) {
-    leaderboard = await getLeaderboardFromDB();
-
-    res.json({ message: "Leaderboard updated.", location: "/leaderboard" });
-});
+async function getPlayerDataFromDb (name) {
+    const leaderboard = await getLeaderboardFromDB()
+    return leaderboard.find((player) => player.displayName.S === name)
+}
 
 module.exports = router;
