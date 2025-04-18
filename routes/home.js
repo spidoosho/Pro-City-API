3f3e2da7faa7dac40535e99c027912570c8f9313
Hoang Anh Tuan <hoang.dusan@gmail.com>
2025-04-18 16:00:12 +0200

    fixed healthcheck

--- routes/home.js
+++ routes/home.js
@@ -4,19 +4,12 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 const router = Router()
-const dbclient = getClient()
 
 /**
  * Sends a JSON response with a string of a player retrieved based on username
  */
 router.get('/healthcheck', async function (req, res) {
-  const hasFound = await getPotatoes(dbclient)
-
-  if (hasFound) {
-    res.json('Leaderboard table found. API is ready to use.')
-  } else {
-    res.json('Could not find Leaderboard table. Please contact the developer.')
-  }
+  res.json('Ready')
 })
 
 export default router
